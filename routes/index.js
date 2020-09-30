fb960b3d05b2a55ee05164fa4c84d8d6f89f2a34
mmboyce <mathieumimmsboyce@gmail.com>
2020-09-30 15:45:58 -0400

    Implement Error Handling on Endpoint.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,39 @@
 const express = require('express');
+const { compareVersions } = require('../service/compareVersions');
+const { route } = require('./users');
 
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.send({
+    endpoints: {
+      compareVersions: '/compareVersions/',
+    },
+  });
+});
+
+router.get('/compareVersions', (req, res) => {
+  res.send({
+    params: '/:versionNumber/:compareToThisVersionNumber',
+  });
+});
+
+router.get('/compareVersions/:versionOne/:versionTwo', (req, res) => {
+  const { versionOne, versionTwo } = req.params;
+
+  try {
+    compareVersions(versionOne, versionTwo);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+
+  res.send(
+    {
+      result:
+        compareVersions(versionOne, versionTwo),
+    },
+  );
 });
 
 module.exports = router;
