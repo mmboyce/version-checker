dab920afe204ee2e5330eb3eb79d71919890d5a7
mmboyce <mathieumimmsboyce@gmail.com>
2020-09-30 12:44:28 -0400

    Implement version comparisons.

---
+++ service/compareVersions.js
@@ -0,0 +1,48 @@
+/**
+ *
+ *
+ * @param {string} versionNumber1 The version number to compare to the second one.
+ * @param {string} versionNumber2 The version to be compared to the second one.
+ * @returns {string} Either "before", "equal", or "after" representing versionNumber1's
+ *                   comparision to versionNumber2
+ */
+const compareVersions = (versionNumber1, versionNumber2) => {
+  const responses = {
+    before: 'before',
+    equal: 'equal',
+    after: 'after',
+  };
+
+  const splitVersionNumber1 = versionNumber1.split('.');
+  const splitVersionNumber2 = versionNumber2.split('.');
+
+  const length1 = splitVersionNumber1.length;
+  const length2 = splitVersionNumber2.length;
+
+  const maxLength = length1 > length2 ? length1 : length2;
+
+  // adjusting for different lengths of verison numbers
+  for (let i = 0; i < maxLength; i += 1) {
+    if (splitVersionNumber1[i] === undefined) {
+      splitVersionNumber1[i] = 0;
+    }
+
+    if (splitVersionNumber2[i] === undefined) {
+      splitVersionNumber2[i] = 0;
+    }
+  }
+
+  for (let i = 0; i < maxLength; i += 1) {
+    if (splitVersionNumber1[i] > splitVersionNumber2[i]) {
+      return responses.after;
+    }
+
+    if (splitVersionNumber1[i] < splitVersionNumber2[i]) {
+      return responses.before;
+    }
+  }
+
+  return responses.equal;
+};
+
+exports.compareVersions = compareVersions;
--- test/service/compareVersions.test.js
+++ test/service/compareVersions.test.js
@@ -1,3 +1,5 @@
+const { compareVersions } = require('../../service/compareVersions');
+
 describe('compareVersions', () => {
   describe('when versions have same amount of periods', () => {
     it('should represent lower numbers as before', () => {
@@ -5,13 +7,13 @@ describe('compareVersions', () => {
       expect(compareVersions('1.0.0', '2.0.0')).toBe('before');
       expect(compareVersions('1.2.0', '3.10.1')).toBe('before');
     });
-    
+
     it('should represent greater numbers as after', () => {
       expect(compareVersions('2.0.0', '1.0.1')).toBe('after');
       expect(compareVersions('1.120.0', '1.12.1')).toBe('after');
       expect(compareVersions('3.0.5', '3.0.1')).toBe('after');
     });
-    
+
     it('should represent same numbers as equal', () => {
       expect(compareVersions('2.0.0', '2.0.0')).toBe('equal');
       expect(compareVersions('1.0.1', '1.0.1')).toBe('equal');
@@ -25,18 +27,17 @@ describe('compareVersions', () => {
       expect(compareVersions('1.1', '1.10.0')).toBe('before');
       expect(compareVersions('1', '1.0.1')).toBe('before');
     });
-    
+
     it('should represent greater numbers as after', () => {
       expect(compareVersions('2.0.0', '1.0')).toBe('after');
       expect(compareVersions('1.1.0', '1.0')).toBe('after');
       expect(compareVersions('2.5.0.12', '2.5.0')).toBe('after');
     });
-    
+
     it('should represent same numbers as equal', () => {
       expect(compareVersions('2', '2.0.0')).toBe('equal');
       expect(compareVersions('1.12', '1.12.0')).toBe('equal');
       expect(compareVersions('2.3.0', '2.3')).toBe('equal');
     });
   });
-
 });
